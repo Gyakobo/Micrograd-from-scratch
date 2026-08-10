84f458a996319f515196c6613717901e0f834eb6
Gyakobo <andygyakobo@gmail.com>
2026-08-10 00:57:59 -0400

    Attempting to implement back propagation

--- main.py
+++ main.py
@@ -10,37 +10,6 @@ def f(x):
     return (3 * x**2) - (4 * x) + 5
 
 
-h = 0.001
-
-a = Value(2.0, label="a")
-b = Value(-3.0, label="b")
-c = Value(10.0, label="c")
-e = a * b
-e.label = "e"
-d = e + c
-d.label = "d"
-f = Value(-2.0, label="f")
-L = d * f
-L.label = "L"
-L1 = L.data  # This is a value node
-
-
-a = Value(2.0, label="a")
-b = Value(-3.0, label="b")
-c = Value(10.0, label="c")
-e = a * b
-e.label = "e"
-d = e + c
-d.label = "d"
-f = Value(-2.0, label="f")
-L = d * f
-L.label = "L"
-L2 = L.data + h  # This is a value node
-
-L.grad = 1
-f.grad = 4
-d.grad = -2
-
 # print((L2 - L1) / h)
 # visualize(draw_bot(L))
 
@@ -52,7 +21,7 @@ x2 = Value(0.0, label="x2")
 w1 = Value(-3.0, label="w1")
 w2 = Value(1.0, label="w2")
 # bias of the neuron
-b = Value(6.7, label="b")
+b = Value(6.8813735870195432, label="b")
 
 x1w1 = x1 * w1
 x1w1.label = "x1*w1"
@@ -67,4 +36,10 @@ n.label = "n"
 
 o = n.tanh()
 
-visualize(draw_bot(n))
+# Gradients
+o.grad = 1
+o._backward()
+n._backward()
+b._backward()
+
+visualize(draw_bot(o))
