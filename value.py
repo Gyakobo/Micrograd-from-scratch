488b8d510568d8c146ee59fc60f369f883e65050
Gyakobo <andygyakobo@gmail.com>
2026-08-11 23:44:34 -0400

    Still working on the operators

--- value.py
+++ value.py
@@ -44,8 +44,7 @@ class Value:
         out = Value(self.data**other, (self,), f"**{other}")
 
         def _backward():
-            self.grad += other.data * out.grad
-            other.grad += self.data * out.grad
+            self.grad += other * (self.data ** (other - 1)) * out.grad
 
         out._backward = _backward
 
