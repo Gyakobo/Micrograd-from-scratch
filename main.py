7fc2edf699a82560349d2d927dbb05f14e6cfd8d
Gyakobo <andygyakobo@gmail.com>
2026-08-12 00:07:17 -0400

    Finally wrapping up with the operators

--- main.py
+++ main.py
@@ -34,7 +34,12 @@ x1w1x2w2.label = "x1*w1 + x2*w2"
 n = x1w1x2w2 + b
 n.label = "n"
 
-o = n.tanh()
+# ------------
+# o = n.tanh()
+e = (2 * n).exp()
+o = (e - 1) / (e + 1)
+o.label = "o"
+# ------------
 
 o.backward()
 visualize(draw_bot(o))
